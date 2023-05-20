895d8039c1fae9589c7422673465dcb111cf610d
phong <phongpn@proxglobal.com>
2023-05-20 17:45:55 +0700

    update gitingore, chang port 8080

--- src/main/java/com/app/music_application/MusicApplication.java
+++ src/main/java/com/app/music_application/MusicApplication.java
@@ -2,12 +2,18 @@ package com.app.music_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class MusicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MusicApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return "Hello World";
 	}
 
 }
