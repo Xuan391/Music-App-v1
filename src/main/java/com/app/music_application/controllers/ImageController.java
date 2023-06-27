0bf85fff046d098de8c9dfff779ef6004c9b16ab
XuanOcCho <thanhxuan090301@gmail.com>
2023-06-27 21:49:05 +0700

    upload image

---
+++ src/main/java/com/app/music_application/controllers/ImageController.java
@@ -0,0 +1,68 @@
+package com.app.music_application.controllers;
+
+import com.app.music_application.models.ResponseObject;
+import com.app.music_application.services.ImageStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/image")
+public class ImageController {
+    @Autowired
+    private ImageStorageService imageStorageService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<ResponseObject> uploadImage(@RequestParam("file") MultipartFile file){
+        try {
+        String generatedFileName = imageStorageService.storeFile(file);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok", "upload file successfully", generatedFileName)
+        );
+    }catch (Exception exception) {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(
+                new ResponseObject("ok", exception.getMessage(), "")
+        );
+    }
+    }
+
+    @GetMapping("/files/{fileName:.+}")
+    // /files/06a290064eb94a02a58bfeef36002483.png
+    public ResponseEntity<byte[]> readDetailFile(@PathVariable String fileName) {
+        try {
+            byte[] bytes = imageStorageService.readFileContent(fileName);
+            return ResponseEntity
+                    .ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(bytes);
+        }catch (Exception exception) {
+            return ResponseEntity.noContent().build();
+        }
+    }
+    //How to load all uploaded files ?
+    @GetMapping("")
+    public ResponseEntity<ResponseObject> getUploadedFiles() {
+        try {
+            List<String> urls = imageStorageService.loadAll()
+                    .map(path -> {
+                        //convert fileName to url(send request "readDetailFile")
+                        String urlPath = MvcUriComponentsBuilder.fromMethodName(ImageController.class,
+                                "readDetailFile", path.getFileName().toString()).build().toUri().toString();
+                        return urlPath;
+                    })
+                    .collect(Collectors.toList());
+            return ResponseEntity.ok(new ResponseObject("ok", "List files successfully", urls));
+        }catch (Exception exception) {
+            return ResponseEntity.ok(new
+                    ResponseObject("failed", "List files failed", new String[] {}));
+        }
+    }
+
+}
