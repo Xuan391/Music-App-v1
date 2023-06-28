fcef8dba7284834c819d9284d5fb2e40015a902d
XuanOcCho <thanhxuan090301@gmail.com>
2023-06-28 16:06:11 +0700

    config

---
+++ src/main/java/com/app/music_application/config/webconfig.java
@@ -0,0 +1,18 @@
+package com.app.music_application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class webconfig {
+    @Bean
+    public WebMvcConfigurer corsConfigureer() {
+        return  new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+            }
+        };
+    }
+}
