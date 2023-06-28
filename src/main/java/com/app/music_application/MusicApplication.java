cf5a51ec37b97ac70cb6ef90a0c41fda595559d2
XuanOcCho <thanhxuan090301@gmail.com>
2023-06-28 16:42:27 +0700

    config

--- src/main/java/com/app/music_application/MusicApplication.java
+++ src/main/java/com/app/music_application/MusicApplication.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MusicApplication {
@@ -11,6 +13,15 @@ public class MusicApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MusicApplication.class, args);
 
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigureer() {
+		return  new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+			}
+		};
 	}
 
 }
