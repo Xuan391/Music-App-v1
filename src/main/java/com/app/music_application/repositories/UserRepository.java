957e235a22d93acf38c1986ff0ead4a28d7e1ad7
phong <phongpn@proxglobal.com>
2023-07-02 18:00:16 +0700

    fix users

--- src/main/java/com/app/music_application/repositories/UserRepository.java
+++ src/main/java/com/app/music_application/repositories/UserRepository.java
@@ -10,12 +10,12 @@ import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByUserName(String userName);
-    @Query(value = "SELECT * FROM  User WHERE name LIKE %:searchText%", nativeQuery = true)
+    @Query(value = "SELECT * FROM  users WHERE name LIKE %:searchText%", nativeQuery = true)
     List<User> searchUsersByName(@Param("searchText") String searchText);
 
-    @Query(value = "SELECT * FROM user WHERE user_name LIKE :username AND password like :password AND is_admin = false", nativeQuery = true)
+    @Query(value = "SELECT * FROM users WHERE user_name LIKE :username AND password like :password AND is_admin = false", nativeQuery = true)
     List<User> checkLogin(@Param("username") String username, @Param("password") String password);
 
-    @Query(value = "SELECT * FROM user WHERE user_name LIKE :username AND password LIKE :password AND is_admin = true", nativeQuery = true)
+    @Query(value = "SELECT * FROM users WHERE user_name LIKE :username AND password LIKE :password AND is_admin = true", nativeQuery = true)
     List<User> findAdminUsers(@Param("username") String username, @Param("password") String password);
 }
